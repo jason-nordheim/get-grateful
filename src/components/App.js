f98734f06eaec476993d4d707170409133f8bdd6
jason-nordheim <jason.nordheim@gmail.com>
2020-10-16 22:55:28 -0600

    stubbed out App component and added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header/>
+      {/* 
+      logged in? 
+        --> feed 
       
+      Unauthenticated
+        --> Login scrren 
+      */}
     </div>
   );
 }
