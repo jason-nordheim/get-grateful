b8f8743b42dcc9b49bed33e0cf4868b4cafa74d9
jason-nordheim <jason.nordheim@gmail.com>
2020-10-16 22:55:06 -0600

    outlined journal component

---
+++ src/components/Journal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/**
+ * The Form Component for creating a new jounral 
+ */
+function Journal() {
+  return (
+    <div className="journal">
+      {/* weather field  */}
+      {/* data field  */}
+      {/* form for jounral entry  */}
+      {/* tags for journal entry */}
+    </div>
+  )
+}
+
+export default Journal
