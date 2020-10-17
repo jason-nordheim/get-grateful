1da4983b432048860751a53ab0f0e6e36183728a
jason-nordheim <jason.nordheim@gmail.com>
2020-10-17 10:38:02 -0600

    added styling to existing components and logic for displaying different components depending on the auth state

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import { LoaderProvider, useLoading, BallTriangle } from '@agney/react-loading';
+
 import Header from './Header'
+import Feed from './Feed'
+
+import { useAuth } from '../hooks/useAuth'
+import Login from './Login'
+
+// function App() {
+//   const { containerProps, indicatorEl } = useLoading({
+//     loading: true,
+//   });
+//   return <section {...containerProps}>{indicatorEl}</section>;
+// }
+
 
 function App() {
+  const [user, loading, error] = useAuth()
   return (
     <div className="app">
-      <Header/>
-      {/* 
-      logged in? 
-        --> feed 
+      <Header />
+      <main>
+      {
+        loading // display animated loader if loading 
+          ? <BallTriangle /> 
+          : error  // display any errors if something goes wrong authenticating with firebase 
+            ? <p>{error} </p> 
+            : user 
+              ? <Feed /> 
+              : <Login /> 
+              /* 
+              /* 
+              logged in? 
+                --> feed 
+              
+              Unauthenticated
+                --> Login scrren 
+              */
+      }
+      </main>
       
-      Unauthenticated
-        --> Login scrren 
-      */}
     </div>
   );
 }
